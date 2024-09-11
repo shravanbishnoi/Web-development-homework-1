c6cbcb3f41a4fced97b8c5290f3a2d267454c5d6
narayan-jat <narayanjat2964@gmail.com>
2024-09-11 23:11:42 +0530

    fix bot profile

--- bot-app/src/ChatPlatform.jsx
+++ bot-app/src/ChatPlatform.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './ChatPlatform.css'
 import MessageInput from './Components/MessageInput'
 import BotProfile from './Components/BotProfile'
+import Message from './Components/Message'
 
 function ChatPlatform() {
 
@@ -12,6 +13,7 @@ function ChatPlatform() {
 
     </div>
       <BotProfile/>
+      <Message/>
     </>
   )
 }
--- bot-app/src/Components/BotProfile.jsx
+++ bot-app/src/Components/BotProfile.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import React from "react";
 import "../Css/BotProfile.css";
+import { format } from "date-fns";
+import { FaUserCircle } from "react-icons/fa";
 function BotProfile() {
-
+  const currentDate = new Date();
+  const formattedDate = format(currentDate, "dd MMM yyyy, hh:mm a");
   return (
-    <div className='bot-profile'>
-        <div className='bot-profile-image'>
-            <img className='bot-profile-image' src="/assets/profile.jpg"></img>
-        </div>
-        <div className='bot-name'>TextChanger</div>
-        <div className='bot-username'>textchanger@dev</div>
-        <div className='bot-description'>Hello this is bot which respond with random texts.</div>
+    <div className="bot-profile">
+      <div className="bot-profile-image">
+      <FaUserCircle className="bot-profile-image" size={40} />
+      </div>
+      <div className="bot-name">TextChanger</div>
+      <div className="bot-username">textchanger@dev</div>
+      <div className="bot-description">
+        Hello this is bot which respond with random texts.
+      </div>
+      <div className="date-line">
+        <span className="line"></span>
+        <span className="date">{formattedDate}</span>
+        <span className="line"></span>
+      </div>
     </div>
-  )
+  );
 }
 
 export default BotProfile;
