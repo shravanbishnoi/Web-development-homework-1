e0116a6f8654c79a4574561cfa5a16da5e4a8c2e
shravanbishnoi <shravanbishnoi008754@gmail.com>
2024-09-11 22:54:02 +0530

    Complete input component

--- bot-app/src/Components/MessageInput.jsx
+++ bot-app/src/Components/MessageInput.jsx
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaRocket, FaSmile } from 'react-icons/fa';
+import EmojiPicker from 'emoji-picker-react';
 import '../Css/MessageInput.css';
 
 function MessageInput() {
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+  const [message, setMessage] = useState('');
+
+  const handleEmojiClick = (emojiObject) => {
+    setMessage(prevMessage => prevMessage + emojiObject.emoji);
+    setShowEmojiPicker(false); // Hide emoji picker after selection
+  };
+
+  const handleSubmit = () => {
+    console.log("Message sent:", message);
+    setMessage(''); // Clear the input field after sending
+  };
+
   return (
     <div className="input-container">
-        <span className="emoji">&#128515;</span> {/* Example emoji */}
-        <input
-            type="text"
-            className="input-field"
-            placeholder="Enter your text..."
-        />
+      <div className="emoji-container">
+        <FaSmile className="emoji-icon" onClick={() => setShowEmojiPicker(!showEmojiPicker)} />
+        {showEmojiPicker && (
+          <div className="emoji-picker">
+            <EmojiPicker onEmojiClick={handleEmojiClick} />
+          </div>
+        )}
+      </div>
+      <input
+        type="text"
+        className="input-field"
+        placeholder="Enter your text..."
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button className="send-button" onClick={handleSubmit}>
+        <FaRocket className="horizontal-plane-icon" />
+      </button>
     </div>
   );
 }
