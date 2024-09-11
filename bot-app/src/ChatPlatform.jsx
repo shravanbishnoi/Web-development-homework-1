66055db5f7694487abc1b4bc66df39c82a787d49
narayan-jat <narayanjat2964@gmail.com>
2024-09-11 20:55:27 +0530

    add file structure

---
+++ bot-app/src/ChatPlatform.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './ChatPlatform.css'
+
+function ChatPlatform() {
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default ChatPlatform
