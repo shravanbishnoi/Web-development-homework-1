38e44a2dbda064b209160827102b09e3ffd4fbfe
narayan-jat <narayanjat2964@gmail.com>
2024-09-11 21:31:49 +0530

    add bot profile component

--- bot-app/src/ChatPlatform.jsx
+++ bot-app/src/ChatPlatform.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './ChatPlatform.css'
+import BotProfile from './Components/BotProfile'
 
 function ChatPlatform() {
 
   return (
     <>
-
+      <BotProfile/>
     </>
   )
 }
