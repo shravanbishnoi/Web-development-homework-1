c04660081cdff61824e4d4c8b0778df683157463
narayan-jat <narayanjat2964@gmail.com>
2024-09-12 11:33:49 +0530

    modify navbar

--- bot-app/src/ChatPlatform.jsx
+++ bot-app/src/ChatPlatform.jsx
@@ -1,17 +1,20 @@
-import { useState } from 'react'
-import './ChatPlatform.css'
-import MessageInput from './Components/MessageInput'
-import BotProfile from './Components/BotProfile'
-import Message from './Components/Message'
-import ChatNavbar from './Components/ChatNavbar'
+import { useState } from "react";
+import "./ChatPlatform.css";
+import MessageInput from "./Components/MessageInput";
+import BotProfile from "./Components/BotProfile";
+import Message from "./Components/Message";
+import ChatNavbar from "./Components/ChatNavbar";
 
 function ChatPlatform() {
-
   return (
-    <>
-    <ChatNavbar/>
-    </>
-  )
+    <div className="bot">
+      <div>
+        <ChatNavbar />
+      </div>
+      <div className="message-window"></div>
+      <div className="chat-input"><MessageInput /></div>
+    </div>
+  );
 }
 
-export default ChatPlatform
+export default ChatPlatform;
