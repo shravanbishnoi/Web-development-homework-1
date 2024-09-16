90a17e6c1f1a0437b4169d04d201a4fcc6ce2da3
narayan-jat <narayanjat2964@gmail.com>
2024-09-16 15:10:22 +0530

    add bot header on message

--- bot-app/src/Components/Message.jsx
+++ bot-app/src/Components/Message.jsx
@@ -2,17 +2,22 @@ import React from "react";
 import { format } from "date-fns";
 import "../Css/Message.css";
 
-function Message({message}) {
-
+function Message({ message }) {
   const currentDate = new Date();
   const formattedDate = format(currentDate, "h:mm a");
 
   return (
-    <div className={`message ${message.sender !== 'human' ? 'bot' : 'human'}`}>
-      <div className="message-body"><p style={{textAlign: "justify"}}>{message.text}</p></div>
-      <div className="message-header message-timeline">
-          {formattedDate}
+    <div className={`message ${message.sender !== "human" ? "bot" : "human"}`}>
+      <div
+        className="message-header"
+        style={{ display: message.sender !== "human" ? "block" : "none" }}
+      >
+        {message.sender}
       </div>
+      <div className="message-body">
+        <p style={{ textAlign: "justify" }}>{message.text}</p>
+      </div>
+      <div className="message-timeline">{formattedDate}</div>
     </div>
   );
 }
